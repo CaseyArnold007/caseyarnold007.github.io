8a0eefcbb48da280dfb17f0fe23d33a44a8beaba
CaseyArnold007 <caseyarnold567@gmail.com>
2015-02-18 01:54:47 -0600

    Changing Pizza Background

--- views/js/main.js
+++ views/js/main.js
@@ -483,55 +483,42 @@ var items = document.getElementsByClassName('mover'),
     lastScrollY = 0,
     ticking = false;
 
-// Callback for our scroll event - just
-// keeps track of the last scroll value
-function onScroll() {
-    lastScrollY = window.scrollY;
-    requestTick();
-}
-
-// Calls rAF if it's not already been done already
-function requestTick() {
-    if(!ticking) {
-        window.requestAnimationFrame(updatePositions);
-        ticking = true;
-    }
-}
-
 function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  // Optimized with transform3d and took some objects out from the loop
-  var phase = Math.sin(document.body.scrollTop / 1250);
+  var items = document.querySelectorAll('.mover');
   for (var i = 0; i < items.length; i++) {
-    items[i].style.transform = 'translate3d(' + ((i % 8) * 256 + 100 * (phase + (i % 5))) + 'px, 0px, 0px)';
+    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
+  // User Timing API to the rescue again. Seriously, it's worth learning.
+  // Super easy to create custom metrics.
   window.performance.mark("mark_end_frame");
   window.performance.measure("measure_frame_duration", "mark_start_frame", "mark_end_frame");
   if (frame % 10 === 0) {
     var timesToUpdatePosition = window.performance.getEntriesByName("measure_frame_duration");
     logAverageFrame(timesToUpdatePosition);
   }
-  ticking = false;
 }
 
-// Optimized with new scroll tracer
-window.addEventListener('scroll', onScroll, false);
+// runs updatePositions on scroll
+window.addEventListener('scroll', updatePositions);
 
-// Optimized with appropriate image size and rAF
-// Moved duplicate CSS attributes to style section in index.html
-document.addEventListener('DOMContentLoaded', 
-  window.requestAnimationFrame(function() {
+// Generates the sliding pizzas when the page loads.
+document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
   for (var i = 0; i < 32; i++) {
     var elem = document.createElement('img');
-    elem.classList.add('mover');
-    elem.src = "images/pizza_b.png";
+    elem.className = 'mover';
+    elem.src = "images/pizza_s.png";
+    elem.style.height = "100px";
+    elem.style.width = "73.333px";
+    elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    document.getElementById("movingPizzas1").appendChild(elem);
+    document.querySelector("#movingPizzas1").appendChild(elem);
   }
-  window.requestAnimationFrame(updatePositions);
-}));
+  updatePositions();
+});
